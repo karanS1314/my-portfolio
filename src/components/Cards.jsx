39bee9dc160eb708682419710895f7163abd3603
Gurkaran Singh <gurkaran1314@gmail.com>
2024-10-03 01:27:29 +0530

    added showDescription for cards section

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import braneImg from "../assets/images/brane.png";
 import citymallImg from "../assets/images/citymall.png";
 import iitrImg from "../assets/images/iitr.png";
+
 function Cards() {
+  const [showDescription, setShowDescription] = useState(null); // Changed to store card identifier
+
+  const handleShowDescription = (id) => {
+    setShowDescription(showDescription === id ? null : id); // Toggle logic based on id
+    console.log(showDescription);
+  };
+
+  const handleMouseLeave = () => {
+    setShowDescription(null); // Reset on mouse leave
+  };
+
   return (
     <div
       data-scroll
@@ -15,7 +28,11 @@ function Cards() {
         </h1>
       </div>
       <div className="container w-full flex flex-col md:flex-row gap-5 py-20 text-[#E3E3DE]">
-        <div className="cursor-pointer hover:scale-105 transition-all duration-300 card relative flex flex-col gap-5  items-center py-12 lg:w-1/2 md:w-full h-80 md:h-96 bg-[#35008B] rounded-xl">
+        <div
+          className="cursor-pointer hover:scale-105 transition-all duration-300 card relative flex flex-col gap-5  items-center py-12 lg:w-1/2 md:w-full h-80 md:h-96 bg-[#35008B] rounded-xl "
+          onMouseEnter={() => handleShowDescription("brane")}
+          onMouseLeave={handleMouseLeave} // Reset on mouse leave
+        >
           <img
             src={braneImg}
             alt="BRANE ENTERPISES"
@@ -24,7 +41,9 @@ function Cards() {
           <p className="text-lg -mt-1 sm:mt-0 font-semibold">
             Software Developer(ASL)
           </p>{" "}
-          <p className="text-md -mt-4 sm:mt-0 px-5 lg:px-12 leading-6 lg:leading-7 transition-opacity duration-500 opacity-0 hover:opacity-100">
+          <p
+            className={`text-md -mt-4 sm:mt-0 px-5 lg:px-12 leading-6 lg:leading-7 transition-opacity duration-500 ${showDescription === "brane" ? "opacity-100" : "opacity-0"}`}
+          >
             {/* Added animation on hover */}
             Led development on the NSL platform and Stringer Chatbot, optimizing
             APIs and enhancing UX with ReactJS, RESTful APIs, and batch
@@ -34,7 +53,11 @@ function Cards() {
             2024
           </button>
         </div>
-        <div className="cursor-pointer hover:scale-105 lg:w-1/2 w-full transition-all duration-300 card relative flex flex-col gap-5 items-center  py-12 h-80 md:h-96 bg-[#E71053] rounded-xl">
+        <div
+          className="cursor-pointer hover:scale-105 lg:w-1/2 w-full transition-all duration-300 card relative flex flex-col gap-5 items-center py-12 h-80 md:h-96 bg-[#E71053] rounded-xl"
+          onMouseEnter={() => handleShowDescription("citymall")}
+          onMouseLeave={handleMouseLeave} // Reset on mouse leave
+        >
           <img
             src={citymallImg}
             alt="CITYMALL"
@@ -43,7 +66,9 @@ function Cards() {
           <p className="text-lg -mt-1 sm:mt-0 font-semibold">
             Software Engineer
           </p>{" "}
-          <p className="text-md px-5 -mt-4 sm:mt-0 lg:px-12 leading-6 lg:leading-7 transition-opacity duration-500 opacity-0 hover:opacity-100">
+          <p
+            className={`text-md px-5 -mt-4 sm:mt-0 lg:px-12 leading-6 lg:leading-7 transition-opacity duration-500 ${showDescription === "citymall" ? "opacity-100" : "opacity-0"}`}
+          >
             {/* Added animation on hover */}
             Developed responsive UI for WMS using React Native, implemented
             delivery route optimization, and reduced bugs through thorough code
